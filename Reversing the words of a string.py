bec3d75d670851394f8b5f61fc89fc4917ac7e30
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-09-03 10:19:23 +0530

    Reverse a String.

---
+++ Reversing the words of a string.py
@@ -0,0 +1,35 @@
+#Without using Array or any function.
+
+S = " Sky is     Blue   "
+temp = ""
+final = ""
+for s in range(len(S)-1 , -1 , -1):
+    if S[s] != " ":
+        temp += S[s]
+    if len(temp) > 0 and S[s] == " ":
+        for t in range(len(temp)-1 , -1 , -1):
+            final += temp[t]
+        final += " "
+        temp = ""
+print( "Original String is >>" , S)
+print("After reversing the words , string is >>" ,final)
+
+
+#Using functions and array
+
+String = "Sky is Blue"
+String = String.split()
+String.reverse()
+Final = ""
+for s in String:
+    Final += s 
+    Final += " "
+print(Final)
+
+
+        
+    
+
+        
+
+
